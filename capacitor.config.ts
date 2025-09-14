33e38d418b722face0801e7a29567d560511633e
Zuanzi <70107300+faizinuha@users.noreply.github.com>
2025-09-14 11:59:13 +0000

    Up

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.StarMar',
+    appId: 'com.StarMar',
   appName: 'StarMar',
-  webDir: 'www'
+  webDir: 'dist'
 };
 
 export default config;
