06dccf8a374d4e0538ee006eff13549704780c3a
Zuanzi <70107300+faizinuha@users.noreply.github.com>
2025-10-10 11:55:26 +0000

    Up

--- supabase/functions/spotify-track-by-id/index.ts
+++ supabase/functions/spotify-track-by-id/index.ts
@@ -1,4 +1,4 @@
-import { serve } from 'https://deno.land/std@0.177.0/http/server.ts';
+import { serve } from 'https://deno.land/std@0.177.0/http/server.ts'; // This import is correct for Deno.
 import { corsHeaders } from '../_shared/cors.ts';
 
 /**
