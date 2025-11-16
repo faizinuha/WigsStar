5e2d88d461a601985a95da74ed3b14aab726cec0
Zuanzi <70107300+faizinuha@users.noreply.github.com>
2025-11-16 05:32:07 +0000

    Don;t Ekspect

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap'; // Import integrasi
+
+export default defineConfig({
+  // PASTI BUKA URL INI VALID 100%
+  // Gunakan HTTPS dan pastikan tidak ada garis miring di akhir.
+  site: 'https://starmar2.vercel.app', 
+  
+  integrations: [
+    sitemap(), // Aktifkan integrasi sitemap
+  ],
+  
+  // Jika Anda menggunakan Vercel, pastikan adapter sudah benar
+  // output: 'server',
+  // adapter: vercel(), 
+});
